52b3a3b469d05cda973392a3f59cc26e716516a1
IrhamIzza <IrhamIzza21@gmail.com>
2020-04-16 11:56:04 +0700

    pertemuan 11

---
+++ Pertemuan 11/Jobsheet11/src/Percobaan/MainLinkedLists.java
@@ -0,0 +1,21 @@
+package Percobaan;
+public class MainLinkedLists {
+    public static void main(String[] args) {
+        LinkedList data = new LinkedList();
+    try{
+        data.addFirst(7);
+        data.print();
+        data.add(8, 1);
+        data.print();
+        data.addFirst(1);
+        data.print();
+        data.remove(1);
+        data.print();
+        data.clear();
+        data.print();
+    }catch (Exception e) {
+            System.out.println(e.getMessage());
+    }
+    }
+}
+
